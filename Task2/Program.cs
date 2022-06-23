f03d555d5aa2dd2977187ff60030aef0f37220b4
SergeiEremkin <ffr@list.ru>
2022-06-24 00:59:26 +0300

    add 3 tasks for homework6

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void UserInput()
+{
+    System.Console.WriteLine("Введите b1");
+    int b1 = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Введите k1");
+    int k1 = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Введите b2");
+    int b2 = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Введите b2");
+    int k2 = Convert.ToInt32(Console.ReadLine());
+    ThePointOfiIntersectionLogic(b1,k1,b2,k2);
+}
+
+void ThePointOfiIntersectionLogic(int b1, int k1, int b2, int k2)
+{
+    int x = (b2-b1)/(k1-k2);
+    int y = (k2*x) + b2;
+    System.Console.WriteLine($"Точка пересечения:X = {x} Y = {y}");
+}
+UserInput();
