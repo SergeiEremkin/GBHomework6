a6a69189a4cf433ab31f8f62c3c168456ec96131
SergeiEremkin <ffr@list.ru>
2022-06-24 01:02:28 +0300

    add tasks

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CountPositivNumbers()
+{
+    int[] array = UserInputInterface();
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            counter++;
+        }
+    }
+    System.Console.WriteLine($"Кол-во положительных чисел: {counter}");
+}
+int[] UserInputInterface()
+{
+    System.Console.WriteLine("Сколько чисел хотите ввести?");
+    int M = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[M];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine("Введите число:");
+        int Number = Convert.ToInt32(Console.ReadLine());
+        arr[i] = Number;
+    }
+    return arr;
+}
+CountPositivNumbers();
