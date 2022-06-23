8cbf5f701e7e8db61df42c3b19f06ff31a89b38a
SergeiEremkin <ffr@list.ru>
2022-06-24 01:20:26 +0300

    Исправил 2ое задание

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,20 +1,20 @@
 ﻿void UserInput()
 {
     System.Console.WriteLine("Введите b1");
-    int b1 = Convert.ToInt32(Console.ReadLine());
+    double b1 = Convert.ToDouble(Console.ReadLine());
     System.Console.WriteLine("Введите k1");
-    int k1 = Convert.ToInt32(Console.ReadLine());
+    double k1 = Convert.ToDouble(Console.ReadLine());
     System.Console.WriteLine("Введите b2");
-    int b2 = Convert.ToInt32(Console.ReadLine());
-    System.Console.WriteLine("Введите b2");
-    int k2 = Convert.ToInt32(Console.ReadLine());
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    System.Console.WriteLine("Введите k2");
+    double k2 = Convert.ToDouble(Console.ReadLine());
     ThePointOfiIntersectionLogic(b1,k1,b2,k2);
 }
 
-void ThePointOfiIntersectionLogic(int b1, int k1, int b2, int k2)
+void ThePointOfiIntersectionLogic(double b1, double k1, double b2, double k2)
 {
-    int x = (b2-b1)/(k1-k2);
-    int y = (k2*x) + b2;
+    double x = (b2-b1)/(k1-k2);
+    double y = (k2*x) + b2;
     System.Console.WriteLine($"Точка пересечения:X = {x} Y = {y}");
 }
 UserInput();
